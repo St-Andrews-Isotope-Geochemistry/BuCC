20650d06b38ffe50adde5902816840f03c1c5716
rossidae <rossdmw@gmail.com>
2020-11-25 15:45:26 +0000

    Initial commit with basic functionality to convert measured d11B to borate d11B using polynomial relationship.

---
+++ BoronSpeciesCalibration.m
@@ -0,0 +1,29 @@
+classdef BoronSpeciesCalibration
+    properties
+        d11B_s = delta("B",NaN);
+        d11B_4 = delta("B",NaN);
+        
+        form = "";
+        coefficients = [];
+    end
+    methods
+        function self = BoronSpeciesCalibration(form,coefficients)
+            if nargin==0;
+                self.form = "";
+                self.coefficients = [];
+            else
+                self.form = form;
+                self.coefficients = coefficients;
+            end
+        end
+        function output = translate(self)
+            if self.form=="polynomial" || self.form=="linear"
+                output = 0;
+                for index = 1:numel(self.coefficients)
+                    output = output+self.coefficients(index)*self.d11B_s.value^(numel(self.coefficients)-index);
+                end
+                self.d11B_4.value = output;
+            end
+        end
+    end
+end
