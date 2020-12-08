16f434c4ac802b2024ef7e837085fb1bd74bf01a
rossidae <rossdmw@gmail.com>
2020-12-08 12:02:30 +0000

    Added scaled pressure correction for ks/kf seawater/total scale correction. Also moved object creation into constructor.

--- EquilibriumCoefficient.m
+++ EquilibriumCoefficient.m
@@ -1,9 +1,10 @@
 classdef EquilibriumCoefficient < pX
     properties
-        conditions = Conditions()
+        conditions;
         
         pressure_correction = [NaN,NaN,NaN,NaN,NaN]
         correction = NaN;
+        scaled_correction = NaN;
         
         R_gas = 8.314510;        % J mol-1 deg-1 (perfect Gas)  
         R_P = 83.14472;             % mol bar deg-1 
@@ -19,6 +20,11 @@ classdef EquilibriumCoefficient < pX
         value_no_pressure_correction = NaN;
     end
     methods
+        % Constructor
+        function self = EquilibriumCoefficient();
+            self.conditions = Conditions();
+        end
+        
         % Setters
         function set.temperature(self,value)
             self.conditions.temperature = value;
@@ -58,6 +64,13 @@ classdef EquilibriumCoefficient < pX
             deltaK = self.pressure_correction(4) + self.pressure_correction(5)*self.temperature;
             self.correction = exp(-(deltaV./(self.R_P*(self.temperature+273.15)))*self.pressure + (0.5*deltaK/(self.R_P*(self.temperature+273.15)))*self.pressure^2);
             self.value = self.value*self.correction;
+        end
+        function doScaleCorrectedPressureCorrection(self,scale_correction)
+            deltaV = self.pressure_correction(1) + self.pressure_correction(2)*self.temperature + self.pressure_correction(3)*self.temperature^2;
+            deltaK = self.pressure_correction(4) + self.pressure_correction(5)*self.temperature;
+            self.correction = exp(-(deltaV./(self.R_P*(self.temperature+273.15)))*self.pressure + (0.5*deltaK/(self.R_P*(self.temperature+273.15)))*self.pressure^2);
+            self.scaled_correction = self.correction*scale_correction;
+            self.value = self.value*self.scaled_correction;
         end
     end
 end
