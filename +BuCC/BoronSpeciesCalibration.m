12745c97d7280a8773a8994d80d18e553e82811d
rossidae <rossdmw@gmail.com>
2021-02-03 11:20:20 +0000

    Changed name to d11B_measured, and added calculable property.

--- +BuCC/BoronSpeciesCalibration.m
+++ +BuCC/BoronSpeciesCalibration.m
@@ -1,10 +1,13 @@
 classdef BoronSpeciesCalibration < handle
     properties
-        d11B_s
+        d11B_measured
         d11B_4
         
         form = "";
         coefficients = [];
+    end
+    properties (Dependent=true)
+        calculable
     end
     methods
         function self = BoronSpeciesCalibration(form,coefficients)
@@ -15,15 +18,25 @@ classdef BoronSpeciesCalibration < handle
                 self.form = form;
                 self.coefficients = coefficients;
             end
-            self.d11B_s = Geochemistry_Helpers.delta("B",NaN);
+            self.d11B_measured = Geochemistry_Helpers.delta("B",NaN);
             self.d11B_4 = Geochemistry_Helpers.delta("B",NaN);
         end
-        function output = apply(self)
+        
+        function output = get.calculable(self)
+            if (~isnan(self.d11B_measured) || ~isnan(self.d11B_4)) && (~strcmp(self.form,"")) && ~isempty(self.coefficients)
+                output = true;
+            else
+                output = false;
+            end
+        end
+        
+        function output = calculate(self)            
             if self.form=="polynomial" || self.form=="linear"
                 output = 0;
                 for index = 1:numel(self.coefficients)
-                    output = output+self.coefficients(index)*self.d11B_s.value^(numel(self.coefficients)-index);
+                    output = output+self.coefficients(index)*self.d11B_measured.value^(numel(self.coefficients)-index);
                 end
+                
                 self.d11B_4.value = output;
             end
         end
