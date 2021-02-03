62e3591400c0bd1f0d12d42313dd522deabfb78c
rossidae <rossdmw@gmail.com>
2021-02-03 10:33:29 +0000

    Corrected name in create function, and changed the calculate function so it can work in both directions.

--- +BuCC/d11BCO2.m
+++ +BuCC/d11BCO2.m
@@ -21,16 +21,24 @@ classdef d11BCO2 < handle&Geochemistry_Helpers.Collator
             for self_index = 1:numel(self)
                 self(self_index).species_calibration.apply();
                 self(self_index).carbonate_chemistry.equilibrium_coefficients.calculate();
-                self(self_index).boron.calculate();
-                self(self_index).carbonate_chemistry.calculate();
+                
+                if self(self_index).boron.calculable
+                    self(self_index).boron.calculate();
+                    self(self_index).carbonate_chemistry.calculate();
+                elseif self(self_index).carbonate_chemistry.calculable
+                    self(self_index).carbonate_chemistry.calculate();
+                    self(self_index).boron.calculate();   
+                else
+                    error("a");
+                end
             end
         end
     end
     methods (Static=true)
         function output = create(number)
-            output(number) = d11B_CO2();
+            output(number) = BuCC.d11BCO2();
             for index = 1:number
-                output(index) = d11B_CO2();
+                output(index) = BuCC.d11BCO2();
             end
         end
     end
